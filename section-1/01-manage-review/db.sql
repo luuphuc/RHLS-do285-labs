9e116f33899cc0e1bf11205f584d8865f94f2734
luuphuc <lhhphuc.218@gmail.com>
2022-09-20 22:34:24 +0700

    finalize dockerfile-review

--- section-1/01-manage-review/db.sql
+++ section-1/01-manage-review/db.sql
@@ -1,1 +1,6 @@
-# SQL to inject data into mysql DB
+-- SQL to inject data into mysql DB
+
+CREATE TABLE Item (id INT, description VARCHAR(20), done INT);
+
+INSERT INTO Item (id,description,done) VALUES(1,"Pick up newspaper","0");
+INSERT INTO Item (id,description,done) VALUES(1,"Buy groceries","1");
