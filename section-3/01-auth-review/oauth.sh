dd8cf568815e7d271044395033af9bd406973877
phuclhh <phuclhh@techcombank.com.vn>
2022-09-20 09:58:48 +0700

    finalize auth-review

--- section-3/01-auth-review/oauth.sh
+++ section-3/01-auth-review/oauth.sh
@@ -2,6 +2,8 @@ oc create secret generic htpasswd-default --from-file default_users -n openshift
 
 oc create secret generic auth-review --from-file tmp_users -n openshift-config
 
+oc patch --type=json oauth cluster -p  "$(cat oauth-patch.json)" -o yaml 
+
 oc patch --type=json oauth cluster -p '''
   [{ "op": "add", 
       "path": "/spec/identityProviders/-", 
